0c0a4a83bf279910efb181c4157dc20f2f8ac37b
Tanguy <lorent.tanguy@gmail.com>
2020-12-01 18:21:07 +0100

    J1-Création_des_structures

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
+
+typedef struct Jeton{
+    char couleur; //'B' = blanc, 'N' = noir
+    int coordonnees[2]; // abscisse, ordonné
+}Jeton;
+
+typedef struct Joueur{
+    string nom;
+    Jeton listeJeton[64];
+    int nbJeton;
+}Joueur;
+
+const int MAXLARGEUR = 8;
+typedef struct Jeu{
+    Joueur joueur1;
+    Joueur joueur2;
+    int plateau[MAXLARGEUR][MAXLARGEUR];
+}Jeu;
 
 int main(){
     cout << "Tu sais pas lire en fait !";
