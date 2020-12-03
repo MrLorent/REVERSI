463b7f105bb81f530c823eb0d2a9d6ad751e6550
Tanguy <lorent.tanguy@gmail.com>
2020-12-03 23:21:20 +0100

    J1-Eclatement_du_code

--- main.cpp
+++ main.cpp
@@ -1,28 +1,10 @@
 #include <iostream>
 #include <cstring>
+#include "main.h"
 
 using namespace std;
 
-typedef struct Jeton{
-    char couleur; //'b' = blanc, 'n' = noir
-    int coordonnees[2]; // abscisse, ordonné
-}Jeton;
-
-typedef struct Joueur{
-    string nom;
-    Jeton listeJeton[64];
-    int nbJeton;
-}Joueur;
-
-const int MAXLARGEUR = 8;
-typedef struct Jeu{
-    Joueur joueur1;
-    Joueur joueur2;
-    int plateau[MAXLARGEUR][MAXLARGEUR];
-}Jeu;
-
 /*---------------PROTOTYPES-------------*/
-void initPlateau(char plateau[MAXLARGEUR][MAXLARGEUR]);
 void affichePlateau(char plateau[MAXLARGEUR][MAXLARGEUR]);
 void afficheLigneTransition();
 
---
+++ main.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+typedef struct Jeton{
+    char couleur; //'b' = blanc, 'n' = noir
+    int coordonnees[2]; // abscisse, ordonné
+}Jeton;
+
+typedef struct Joueur{
+    string nom;
+    Jeton listeJeton[64];
+    int nbJeton;
+}Joueur;
+
+const int MAXLARGEUR = 8;
+typedef struct Jeu{
+    Joueur joueur1;
+    Joueur joueur2;
+    int plateau[MAXLARGEUR][MAXLARGEUR];
+}Jeu;
+
+/*---------------PROTOTYPES-------------*/
+void initPlateau(char plateau[MAXLARGEUR][MAXLARGEUR]);
